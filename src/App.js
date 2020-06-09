79c48b0555f5c6b90c60521c5fd53d3ac5a2d770
wadecoplen <wade-coplen@lambdastudents.com>
2020-06-08 19:52:35 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import About from './components/About'
+import Shop from './components/Shop'
+import Nav from './components/Navigation'
+import Home from './components/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route exact path = '/' component = {Home} />
+        <Route exact path = '/about' component = {About} />
+        <Route path = '/shop' component = {Shop} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+
+
+function Navigation() {
+    const navStyle = {
+        color: 'white',
+        fontFamily: 'Roboto'
+    }
+  return (
+    <nav>
+        <h3>Logo</h3>
+            <ul className= 'nav-links'>
+                <Link style= {navStyle} to = '/'>
+                    <li>Home</li>                
+                </Link>
+                <Link style= {navStyle} to= '/about'>
+                    <li>About</li>
+                </Link>
+                <Link style= {navStyle}  to = '/shop'>
+                    <li>Shop</li>
+                </Link>
+            </ul>
+        
+    </nav>
+  );
+}
+
+export default Navigation;
