ddb18c5071d26b4f9fdeda0f919e866cefc7c140
wadecoplen <wade-coplen@lambdastudents.com>
2020-06-08 21:57:40 -0700

    2. GET request made

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,28 @@
 import React, {useState, useEffect} from 'react';
-
+import axios from 'axios'
 
 
 function Shop() {
+    const [response, setResponse] = useState([]);
+
+    useEffect(() => {
+        fetchItems()
+        .then(response => {
+            console.log(response)
+            setResponse(response)
+        })
+    }, []);
+    
+    
+ 
+    const fetchItems = () => axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+       
+        
   return (
-    <div className="Shop">
-     <h1>Shop Page</h1>
+    <div>
+       {response.map(item => {
+           <h1></h1>
+       })}
     </div>
   );
 }
